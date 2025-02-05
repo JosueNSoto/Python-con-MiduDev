6f0be86a6fe6540c26b37aa20b6a06fece1def6d
Josue Soto <zte-2000@hotmail.com>
2025-02-05 00:41:17 -0600

    first commit

---
+++ 01_Basic/01_print.py
@@ -0,0 +1,17 @@
+
+
+"""
+Acá está el repositorio de Github: https://github.com/midudev/curso-python
+"""
+#minúsculas y para separar con guiones bajos: snake_case
+#mayúsculas y sin separar: CamelCase
+#mayúsculas y separar con guiones bajos: SNAKE_CASE
+
+print("Hola Mundo :)")
+#print es una función que recibe parametros y los imprime en la consola
+
+print("Podemos", "escribir", "varios parámetros", "después de la coma")
+
+print("Modificaremos el final de esta impresión","A menos que tenga un separador en el string", sep="---")
+print("Funciona en un string",end=" -> ")
+print("Este es un test para el salto")
