70294be92580bf379a59fcd3e122e4d7da187e07
Josue Soto <zte-2000@hotmail.com>
2025-02-06 10:30:35 -0600

    Types in python

---
+++ 01_Basic/02_types.py
@@ -0,0 +1,9 @@
+"""
+Vamos a ver los tipos de datos básicos en Python
+"""
+#Tenemos strings o cadenas de texto, números enteros, decimales, booleanos, listas, diccionarios, NontType, tuplas y conjuntos
+print("int: ")
+print("100")#Obtenemos un valor y que es un str
+print(type(100))#Obtenemos el tipo de dato 
+print("-5")#Obtenemos un valor y que es un str
+print(type(-5))#Obtenemos el tipo de dato 
