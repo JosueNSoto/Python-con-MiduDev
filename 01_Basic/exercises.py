3655620fbaafe599f6c32709b0c1eaf754763172
Josue Soto <zte-2000@hotmail.com>
2025-02-17 16:43:25 -0600

    NEW: ejercicios finalizados y evaluados

---
+++ 01_Basic/exercises.py
@@ -0,0 +1,70 @@
+###
+# exercises.py
+# Ejercicios para practicar los conceptos aprendidos en las lecciones.
+###
+
+"""print("\nEjercicio 1: Imprimir mensajes")
+print("Escribe un programa que imprima tu nombre y tu ciudad en líneas separadas.")
+
+### Completa aquí
+print("Mi nombre es: Josue;\nVivo en el EDOMEX")"""
+
+print("--------------")
+
+"""print("\nEjercicio 2: Muestra los tipos de datos de las siguientes variables:")
+print("Usa el comando 'type()' para determinar el tipo de datos de cada variable.")
+a = 15
+b = 3.14159
+c = "Hola mundo"
+d = True
+e = None
+
+### Completa aquí
+print(f"El valor de A es: {a}",type(a))
+print(f"El valor de B es: {b}",type(b))
+print(f"El valor de C es: {c}",type(c))
+print(f"El valor de D es: {d}",type(d))
+print(f"El valor de E es: {e}",type(e))
+"""
+
+print("--------------")
+
+"""print("\nEjercicio 3: Casting de tipos")
+print("Convierte la cadena \"12345\" a un entero y luego a un float.")
+print("Convierte el float 3.99 a un entero. ¿Qué ocurre?")
+
+### Completa aquí
+print(int("12345"))
+print(type(int("12345")))
+
+print(float("12345"))
+print(type(float("12345")))
+
+print(f"3.99 se debería convertir en un entero {round(3.99)}")"""
+
+
+print("--------------")
+
+"""print("\nEjercicio 4: Variables")
+print("Crea variables para tu nombre, edad y altura.")
+print("Usa f-strings para imprimir una presentación.")
+
+# "Hola! Me llamo midudev y tengo 39 años, mido 1.70 metros"
+
+### Completa aquí
+nombre = "midudev"
+edad = 39
+estatura = 1.70
+print(f"Welcome to the dev's rift, mi nombre es {nombre}, tengo {edad} años de edad y mi altura es de {estatura} metros")"""
+
+print("--------------")
+
+"""print("\nEjercicio 5: Números")
+print("1. Crea una variable con el número PI (sin asignar una variable)")
+print("2. Redondea el número con round()")
+print("3. Haz la división entera entre el número que te salió y el número 2")
+print("4. El resultado debería ser 1")
+
+### Completa aquí
+pi = 3.1416
+print(round(pi)//2)"""
