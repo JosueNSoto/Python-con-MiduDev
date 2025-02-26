f4d6f5a3895627a9a7bee0a851bb54569ccf26d1
Josue Soto <zte-2000@hotmail.com>
2025-02-25 23:25:25 -0600

    UPDATE: comentarios y listo

--- 02_flow_work/04_list_exercises.py
+++ 02_flow_work/04_list_exercises.py
@@ -76,6 +76,7 @@ print(referencia)"""
 # Ejercicio 6: Ordenar strings sin diferenciar mayúsculas y minúsculas.
 # Crea una lista con las siguientes cadenas: ["Manzana", "pera", "BANANA", "naranja"].
 # Ordena la lista sin diferenciar entre mayúsculas y minúsculas.
-frutas = ["Manzana", "pera", "BANANA", "naranja"]
+
+"""frutas = ["Manzana", "pera", "BANANA", "naranja"]
 frutas.sort()
-print(frutas)
+print(frutas)"""
